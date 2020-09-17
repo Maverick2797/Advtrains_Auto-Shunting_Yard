4f928f7c9bb7fbe73f85c30aa69662934bb28759
Maverick2797 <49946595+Maverick2797@users.noreply.github.com>
2020-09-17 19:40:24 +0800

    Create entry_track.lua
    initial working out of the entry/waiting track logic

---
+++ entry_track.lua
@@ -0,0 +1,44 @@
+local yard = "BY" --change yard id on per-yard basis
+local clockspeed = 5
+y = yard
+
+
+local check_yard_active = function(yard_active,clockspeed)
+	if not atc_id then return end
+	if yard_active == true then
+		atc_send("B0S0")
+		atc_set_text_outside("Waiting for yard to clear")
+		atc_set_text_inside("Waiting for yard to clear")
+		interrupt(clockspeed,"check_active")
+		return
+	else
+		--yard entry procedure
+		--save train info
+		S[y].rc = get_rc() or ""
+		S[y].line = get_line() or ""
+		S[y].yard_active = true
+		
+		if S[y].rc:match(y.."RTS") == true or S[y].single_dir then
+			S[y].RTS = true
+		end
+		atc_set_text_outside("Auto-Shunting Active")
+		atc_set_text_inside("Auto-Shunting Active")
+		set_route(y..dir.."ENTRY","PICKUP")
+		atc_send("S3")
+		return
+	end
+end
+
+if event.train then
+	local rc =  get_rc() or ""
+	if rc:match(yard.."NOSHUNT") then
+		--need to decide how to proceed here
+		return
+	else
+		check_yard_active(S[y].yard_active,clockspeed)
+	end
+end
+
+if event.int then
+	check_yard_active(S[y].yard_active,clockspeed)
+end
