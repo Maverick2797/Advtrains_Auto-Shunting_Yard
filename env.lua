caeabebbf5a5c5429f96fc7d9c8f4acbea8395a7
Maverick2797 <49946595+Maverick2797@users.noreply.github.com>
2020-09-18 21:00:45 +0800

    sort functions alphabetically

--- env.lua
+++ env.lua
@@ -1,36 +1,3 @@
-F.pickup = function(yard,dir,lane)
-	local y = yard
-	if S[y].yard_active then
-		if atc_arrow then
-			if S[y].dir == dir then
-				set_route(yard..dir.."_"..lane, "HEADSHUNT")
-				local plen = train_length()
-				local rc = split_at_fc("B0")
-				local trc = yard..dir.."_"..rc
-				if plen == train_length() and (rc == "" or S[y].rc:match(trc)) then
-					S[y].exiting = true
-				end
-				if rc and rc ~= "" then
-					set_rc(yard..dir.."_"..rc)
-				else
-					set_rc(S[y].rc)
-					S[y].exiting = true
-				end
-			else -- if S[y].dir ~= dir
-				if S[y].RTS then -- loop around to other HS
-					S[y].RTS = nil
-					set_route(yard..dir.."_"..lane,"AROUND")
-					split_off_locomotive("B0")
-					set_rc("AROUND_"..S[y].dir)
-					return
-				else
-					atc_send("B0WRS4")
-				end
-			end
-		end -- nothing to do if train goes against arrow
-	end
-end
-
 F.EOL = function(yard,dir,lane)
 	local y = yard
 	if S[y].yard_active then
@@ -64,5 +31,38 @@ F.exit_set_info = function(yard)
 			S[y].exiting = nil
 			S[y].yard_active = nil
 		end
+	end
+end
+
+F.pickup = function(yard,dir,lane)
+	local y = yard
+	if S[y].yard_active then
+		if atc_arrow then
+			if S[y].dir == dir then
+				set_route(y..dir.."_"..lane, "HEADSHUNT")
+				local plen = train_length()
+				local rc = split_at_fc("B0")
+				local trc = y..dir.."_"..rc
+				if plen == train_length() and (rc == "" or S[y].rc:match(trc)) then
+					S[y].exiting = true
+				end
+				if rc and rc ~= "" then
+					set_rc(y..dir.."_"..rc)
+				else
+					set_rc(S[y].rc)
+					S[y].exiting = true
+				end
+			else -- if S[y].dir ~= dir
+				if S[y].RTS then -- loop around to other HS
+					S[y].RTS = nil
+					set_route(y..dir.."_"..lane,"AROUND")
+					split_off_locomotive("B0")
+					set_rc("AROUND_"..S[y].dir)
+					return
+				else
+					atc_send("B0WRS4") -- train has collected wagons to sort. bounce
+				end
+			end
+		end -- nothing to do if train goes against arrow
 	end
 end
